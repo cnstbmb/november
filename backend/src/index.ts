9e9b1a40e898944a64e4184b1b0574a0e5a000a6
cnstbmb <cnstbmb@gmail.com>
2022-05-23 22:57:34 +0300

    backend: fix styles env.ts; index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
-import {Server} from './server/server'
-import{makeLogger} from './logger/logger-factory'
+import { Server } from './server/server';
+import { makeLogger } from './logger/logger-factory';
 
 const logger = makeLogger();
 const server = new Server(logger);

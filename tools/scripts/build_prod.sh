80195ced933928b1f6553d3a4a5ee60c3e6b513b
cnstbmb <6008378+cnstbmb@users.noreply.github.com>
2022-06-03 17:54:55 +0300

    fix(tools/scripts): fixed script "build:prod"

--- tools/scripts/build_prod.sh
+++ tools/scripts/build_prod.sh
@@ -18,7 +18,7 @@ echo "backend built successful after $((build_backend_time / 60)) minutes and $(
 echo "start building frontend production"
 build_frontend_start_time=`date +%s`
 cd $SCRIPTPATH/../../frontend
-npm run build
+npm run build:prod
 build_frontend_end_time=`date +%s`
 build_frontend_time=$((build_frontend_end_time-build_frontend_start_time))
 echo "frontend built successful after $((build_frontend_time / 60)) minutes and $((build_frontend_time % 60)) seconds."

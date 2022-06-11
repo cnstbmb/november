bb9bdf19f8c1972c44d4e1c4358758464bdff70c
cnstbmb <6008378+cnstbmb@users.noreply.github.com>
2022-06-11 22:30:27 +0300

    fix(frontend): добавлены alt для img ссылок

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 interface Links {
   href: string;
   imgPath: string;
+  imgAlt: string;
 }
 
 @Component({
@@ -14,24 +15,29 @@ export class AppComponent {
   readonly socialLinks: Links[] = [
     {
       href: 'https://vk.com/khimenkov',
-      imgPath: './assets/VK-Monochrom-Logo/Monochrome-White/VK_Monochrome_Compact_Logo.svg'
+      imgPath: './assets/VK-Monochrom-Logo/Monochrome-White/VK_Monochrome_Compact_Logo.svg',
+      imgAlt: 'VK'
     },
     {
       href: 'https://www.facebook.com/k.khimenkov/',
       imgPath:
-        './assets/Facebook-Brand-Asset-Pack-2019/f-Logos-2019-1/f_Logo_Online_04_2019/white/PNG/f_logo_RGB-White_100.png'
+        './assets/Facebook-Brand-Asset-Pack-2019/f-Logos-2019-1/f_Logo_Online_04_2019/white/PNG/f_logo_RGB-White_100.png',
+      imgAlt: 'FB'
     },
     {
       href: 'https://www.instagram.com/k.khimenkov/',
-      imgPath: './assets/Instagram-Logo-Pack/02-White-Glyph/Instagram_Glyph_White.svg'
+      imgPath: './assets/Instagram-Logo-Pack/02-White-Glyph/Instagram_Glyph_White.svg',
+      imgAlt: 'Instagram'
     },
     {
       href: 'https://github.com/cnstbmb',
-      imgPath: './assets/GitHub-Mark/PNG/GitHub-Mark-Light-120px-plus.png'
+      imgPath: './assets/GitHub-Mark/PNG/GitHub-Mark-Light-120px-plus.png',
+      imgAlt: 'GitHub'
     },
     {
       href: 'https://www.linkedin.com/in/khimenkov',
-      imgPath: './assets/LinkedIn-Logos/In/Digital/White/1x/In-White-96.png'
+      imgPath: './assets/LinkedIn-Logos/In/Digital/White/1x/In-White-96.png',
+      imgAlt: 'LinkedIn'
     }
   ];
 }

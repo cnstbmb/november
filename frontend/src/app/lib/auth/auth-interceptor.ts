688ea2170ea41aaed56b348ffb11d14af96e8b27
cnstbmb <6008378+cnstbmb@users.noreply.github.com>
2022-06-28 23:43:59 +0300

    feat(frontend): созданы модули и сервисы для авторизации

---
+++ frontend/src/app/lib/auth/auth-interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthLocalStorage } from '@app/lib/auth/types';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const idToken = localStorage.getItem(AuthLocalStorage.id_token);
+
+    if (!idToken) {
+      return next.handle(req);
+    }
+
+    const cloned = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${idToken}`)
+    });
+
+    return next.handle(cloned);
+  }
+}

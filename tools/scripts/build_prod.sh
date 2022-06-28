ddba7e2c6e239a4208007067560102464e8949ed
cnstbmb <6008378+cnstbmb@users.noreply.github.com>
2022-06-28 23:39:36 +0300

    feat(frontend): созданы модули авторизации и страницы со списком задач
    Настроены роуты
    Сверстана страница авторизации

--- tools/scripts/build_prod.sh
+++ tools/scripts/build_prod.sh
@@ -24,7 +24,7 @@ build_frontend_time=$((build_frontend_end_time-build_frontend_start_time))
 echo "frontend built successful after $((build_frontend_time / 60)) minutes and $((build_frontend_time % 60)) seconds."
 
 echo "start copying angular application to './static' backend"
-cp -r dist/** ../backend/compiled/static
+cp -r dist/** $SCRIPTPATH/../../backend/compiled/static
 
 script_end_time=`date +%s`
 runtime=$((script_end_time-script_start_time))

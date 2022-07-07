d85c600ab6b606d5ae683eab5fa7fde7e41b4a93
cnstbmb <6008378+cnstbmb@users.noreply.github.com>
2022-07-07 16:59:25 +0300

    feat(backend): скрипт для сохранения пользователя в бд
    Установлены пакеты:
     - "prompt"
     - "@types/prompt"
    Исправлен скрипт "migration:create:table" => "migration:create"
    Создан скрипт "user:create"

---
+++ backend/tools/scripts/add-user.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+node $SCRIPTPATH/../../compiled/bin/add-user.js
+#node $SCRIPTPATH/../../compiled/bin/add-user.js | $SCRIPTPATH/../../node_modules/pino-pretty/bin.js --colorize --translateTime

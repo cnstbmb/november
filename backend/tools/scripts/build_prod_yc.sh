270df8d1bdaef6441c976a509523a2f4dbfe5e37
cnstbmb <cnstbmb@gmail.com>
2023-02-05 23:07:28 +0300

    fix(repo): created scripts for yc registry

---
+++ backend/tools/scripts/build_prod_yc.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# TODO: get from env cr.yandex id
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+env
+
+SCRIPT_START_TIME=`date +%s`
+echo "start building backend production docker image"
+
+cd $SCRIPTPATH/../..
+
+TIMESTAMP=$(date +%s)
+IMAGE_NAME="cr.yandex/$YC_REGISTRY_ID/nodejs-server"
+TAG_NEW="$IMAGE_NAME:$TIMESTAMP"
+TAG_LATEST="$IMAGE_NAME:latest"
+
+docker build --no-cache -t $TAG_LATEST -t $TAG_NEW .
+
+docker push $TAG_LATEST
+docker push $TAG_NEW
+
+SCRIPT_END_TIME=`date +%s`
+
+RUNTIME=$((SCRIPT_END_TIME-SCRIPT_START_TIME))
+echo "Building docker image and push time $((RUNTIME / 60)) minutes and $((RUNTIME % 60)) seconds."
